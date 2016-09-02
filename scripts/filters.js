37d080bc03056dd64bdbe949371c5a574ed08c28
Diego Fiore <you@example.com>
2016-09-02 17:56:47 +0100

    Aggiornamento menu, fix pagina competizione e calendario

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,4 +1,4 @@
-﻿angular.module('angularApp.filters', [])
+﻿angular.module('angularApp.filters', ['angularApp.providers'])
     .filter('ageFilter', function () {
         function calculateAge(date, season) { // birthday is a date
             var currentSeason = season.split('-')[0];
@@ -98,25 +98,179 @@
             return text;
         }
     })
-    .filter('groupBy', function ($parse) {
+    .filter('groupBy', function ($parse, filterWatcher) {
         return function (collection, property) {
-            //if (!isUndefined(collection) || isUndefined(property)) {
-            //    return collection;
-            //}
+            
+            if (collection.length === 0) {
+                return collection;
+            }
 
-            var getter = $parse(property);
+            return filterWatcher.isMemoized('groupBy', arguments) ||
+                filterWatcher.memoize('groupBy', arguments, this,
+                groupBy(collection, $parse(property)));
 
-            var result = {};
-            var prop;
+            function groupBy(colletion, property) {
+                var getter = $parse(property);
 
-            collection.forEach(function (elm) {
-                prop = getter(elm);
+                var result = {};
+                var prop;
 
-                if (!result[prop]) {
-                    result[prop] = [];
-                }
-                result[prop].push(elm);
-            });
-            return result;
-    }
-});
+                collection.forEach(function (elm) {
+                    prop = getter(elm);
+
+                    if (!result[prop]) {
+                        result[prop] = [];
+                    }
+                    result[prop].push(elm);
+                });
+                return result;
+            }
+
+        }
+    });
+
+angular.module('angularApp.providers', [])
+  .provider('filterWatcher', function () {
+
+      this.$get = ['$window', '$rootScope', function ($window, $rootScope) {
+
+          /**
+           * Cache storing
+           * @type {Object}
+           */
+          var $$cache = {};
+
+          /**
+           * Scope listeners container
+           * scope.$destroy => remove all cache keys
+           * bind to current scope.
+           * @type {Object}
+           */
+          var $$listeners = {};
+
+          /**
+           * $timeout without triggering the digest cycle
+           * @type {function}
+           */
+          var $$timeout = $window.setTimeout;
+
+          function isScope(obj) {
+              return obj && obj.$evalAsync && obj.$watch;
+          }
+
+          function isNull(value) {
+              return value === null;
+          }
+
+          /**
+           * @description
+           * get `HashKey` string based on the given arguments.
+           * @param fName
+           * @param args
+           * @returns {string}
+           */
+          function getHashKey(fName, args) {
+              function replacerFactory() {
+                  var cache = [];
+                  return function (key, val) {
+                      if (angular.isObject(val) && !isNull(val)) {
+                          if (~cache.indexOf(val)) return '[Circular]';
+                          cache.push(val)
+                      }
+                      if ($window == val) return '$WINDOW';
+                      if ($window.document == val) return '$DOCUMENT';
+                      if (isScope(val)) return '$SCOPE';
+                      return val;
+                  }
+              }
+              return [fName, JSON.stringify(args, replacerFactory())]
+                .join('#')
+                .replace(/"/g, '');
+          }
+
+          /**
+           * @description
+           * fir on $scope.$destroy,
+           * remove cache based scope from `$$cache`,
+           * and remove itself from `$$listeners`
+           * @param event
+           */
+          function removeCache(event) {
+              var id = event.targetScope.$id;
+              angular.forEach($$listeners[id], function (key) {
+                  delete $$cache[key];
+              });
+              delete $$listeners[id];
+          }
+
+          /**
+           * @description
+           * for angular version that greater than v.1.3.0
+           * it clear cache when the digest cycle is end.
+           */
+          function cleanStateless() {
+              $$timeout(function () {
+                  if (!$rootScope.$$phase)
+                      $$cache = {};
+              }, 2000);
+          }
+
+          /**
+           * @description
+           * Store hashKeys in $$listeners container
+           * on scope.$destroy, remove them all(bind an event).
+           * @param scope
+           * @param hashKey
+           * @returns {*}
+           */
+          function addListener(scope, hashKey) {
+              var id = scope.$id;
+              if (angular.isUndefined($$listeners[id])) {
+                  scope.$on('$destroy', removeCache);
+                  $$listeners[id] = [];
+              }
+              return $$listeners[id].push(hashKey);
+          }
+
+          /**
+           * @description
+           * return the `cacheKey` or undefined.
+           * @param filterName
+           * @param args
+           * @returns {*}
+           */
+          function $$isMemoized(filterName, args) {
+              var hashKey = getHashKey(filterName, args);
+              return $$cache[hashKey];
+          }
+
+          /**
+           * @description
+           * store `result` in `$$cache` container, based on the hashKey.
+           * add $destroy listener and return result
+           * @param filterName
+           * @param args
+           * @param scope
+           * @param result
+           * @returns {*}
+           */
+          function $$memoize(filterName, args, scope, result) {
+              var hashKey = getHashKey(filterName, args);
+              //store result in `$$cache` container
+              $$cache[hashKey] = result;
+              // for angular versions that less than 1.3
+              // add to `$destroy` listener, a cleaner callback
+              if (isScope(scope)) {
+                  addListener(scope, hashKey);
+              } else {
+                  cleanStateless();
+              }
+              return result;
+          }
+
+          return {
+              isMemoized: $$isMemoized,
+              memoize: $$memoize
+          }
+      }];
+  });
